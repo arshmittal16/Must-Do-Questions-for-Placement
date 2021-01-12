9796f675ed9e972b54583b40752685f1087603de
arshmittal16 <amittal2_be18@thapar.edu>
2021-01-12 23:56:47 +0530

    Optimised

--- Strings/5. Check if string is rotated by two places.py
+++ Strings/5. Check if string is rotated by two places.py
@@ -34,3 +34,17 @@ def isRotated(s, p):
             return True
 
     return False
+
+## Challenge: Try doing it in O(1) space complexity.
+
+def isRotated(s,p):
+    n = len(s)
+    
+    if (len(s)==len(p)):
+        if (n==1 or n==2) and s==p:
+            return True
+        if n>2:
+            if ((s[2:] == p[:n-2] or s[:n-2] == p[2:])):
+                return True
+
+    return False
